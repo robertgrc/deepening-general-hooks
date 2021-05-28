7a48bf313a6728bd95c5d8629c386b98e5e9f5bf
robertgrc <robert.rodriguez.rgrc@gmail.com>
2021-05-28 18:12:44 -0400

    practice useState

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './counter.css';
 
 export const CounterApp = () => {
+
+
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+        counter4: 40
+    })
+    const {counter1, counter2, counter3, counter4} = state;
+
+
+    // console.log(counter);
     return (
         <>
-           <h1>Counter { 0 }</h1> 
+           <h1>Counter 1 {counter1}</h1>
+           <h1>Counter 2 {counter2}</h1>
+           <h1>Counter 3 {counter3}</h1>
+           <h1>Counter 4 {counter4}</h1>
            <hr />
 
-           <button className="btn btn-primary">
+           <button
+             className="btn btn-primary"
+             onClick = {()=>{
+                 setState({
+                    ...state,
+                    counter1: counter1 + 1
+                 });
+             }}
+            >
                +1
            </button>
         </>
